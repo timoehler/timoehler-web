01d933433df9ca5be1d155b573e49ba0de469abe
Tim Oehler <timoehler@gmail.com>
2019-02-05 16:37:51 -0600

    navbar

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        About Me
+      </h1>
+      <p>
+        My name is Tim.
+      </p>
+    </div>
+  </section>
+</template>
