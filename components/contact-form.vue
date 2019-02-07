45eacd07768ef7d52725121739f45b1f04b23497
Tim Oehler <timoehler@gmail.com>
2019-02-06 21:38:03 -0600

    adding contact form files

---
+++ components/contact-form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <formio
+      src="https://rdoxiwnceknkbun.form.io/contactus?header=0&theme=simplex"
+    />
+  </div>
+</template>
+
+<script>
+import { Form } from 'vue-formio'
+export default {
+  name: 'App',
+  components: { formio: Form }
+}
+</script>
---
+++ plugins/contact-form.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import ContactForm from '~/components/contact-form.vue'
+
+Vue.component('ContactForm', ContactForm)
